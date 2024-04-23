42211f36752bffa42fad398e4292e68bf275b62f
SuvroGfz <1905020@ugrad.cse.buet.ac.bd>
2024-04-23 20:09:22 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/common/Navbar.js";
+import Hero from "./components/homepage/Hero.js";
+import Footer from "./components/common/Footer.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        haloooo
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +21,20 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <button className="btn">Button</button>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-ghost">Ghost</button>
+        <button className="btn btn-link">Link</button>
+      </header> */}
+      <Navbar />
+
+      <Hero />
+
+      <Footer />
     </div>
   );
 }
